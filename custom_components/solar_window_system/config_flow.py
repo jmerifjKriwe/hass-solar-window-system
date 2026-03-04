637a7323386628dd4c7eca1ab987d502e157a1eb
teha <kiesow402@googlemail.com>
2026-03-04 10:30:21 +0100

    style(config_flow): apply ruff formatting

--- custom_components/solar_window_system/config_flow.py
+++ custom_components/solar_window_system/config_flow.py
@@ -32,9 +32,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    async def async_step_user(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Handle the initial step."""
         errors: dict[str, str] = {}
         if user_input is not None:
